eed379db59a41c2ca3f5b97db7c7d00b52e8b613
Gordie Smith <agsmith4@gmail.com>
2014-11-05 22:59:19 +0000

    Created basic working caesar cypher. Need to add all exceptions for non letter characters and for capital latters. Shift function works.

--- cypher.rb
+++ cypher.rb
@@ -1,3 +1,11 @@
-def caesar_cypher(string)
+def caesar(string, key) 
+  new_string = ""
+  letters_in_string = string.split("")
+  letters_in_string.each do |letter|
+    new_ordinal = letter.ord + key
+    new_string << new_ordinal.chr
+  end 
+  puts new_string
 end
-  
+
+caesar("abc", 2)
