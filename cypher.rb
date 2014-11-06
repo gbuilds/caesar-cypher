83cd74a7271c62d8643e9f26015b0e83987616dd
Gordie Smith <agsmith4@gmail.com>
2014-11-06 04:08:01 +0000

    Distinguish between A-Z, a-z, and other characters.

--- cypher.rb
+++ cypher.rb
@@ -2,10 +2,17 @@ def caesar(string, key)
   new_string = ""
   letters_in_string = string.split("")
   letters_in_string.each do |letter|
-    new_ordinal = letter.ord + key
-    new_string << new_ordinal.chr
-  end 
+    if ("a".."z").include? letter
+      new_ordinal = letter.ord + key
+      new_string << new_ordinal.chr
+    elsif ("A".."Z").include? letter
+      new_ordinal = letter.ord + key
+      new_string << new_ordinal.chr
+    else
+      new_string << letter
+    end
+  end
   puts new_string
 end
 
-caesar("abc", 2)
+caesar("abcABC$$$", 5)
